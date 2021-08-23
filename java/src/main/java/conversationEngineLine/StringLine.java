f01363e738f9f37a9d3b6afe53ecc7b8a9840658
sijmen_v_b <sijmenvanbommel@gmail.com>
2021-08-23 02:07:06 +0200

    added mentioning @s to actually mantion the player name

--- java/src/main/java/conversationEngineLine/StringLine.java
+++ java/src/main/java/conversationEngineLine/StringLine.java
@@ -11,14 +11,19 @@ import conversationEngineInporter.NPC;
 public class StringLine extends ConversationLine {
 
 	private String text;
-	
 
 	public StringLine(String text, ConverzationNode node) {
 		super(node);
 		this.text = text;
 	}
 
-	public String toCommand(HashMap<String, ConverzationNode> nodes, CEStory ceStory, NPC npc, LinkedList<String> condition, String con) {
-		return String.format("%srun tellraw @s [{\"text\":\"%s\",\"color\":\"white\"}]\n",con, Functions.stringEscape(text));
+	public String toCommand(HashMap<String, ConverzationNode> nodes, CEStory ceStory, NPC npc,
+			LinkedList<String> condition, String con) {
+		return String.format("%srun tellraw @s [{\"text\":\"%s\",\"color\":\"white\"}]\n", con,
+				Functions.stringEscape(text).replace("@s", "\"},{\"selector\":\"@s\"},{\"text\":\"")); // when you type
+																										// @s it will
+																										// type the name
+																										// of the
+																										// player.
 	}
 }
