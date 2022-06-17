0fc3a7090023c23bc8331e7d59423e1e92f9aef8
raincloudman <rayneman05@gmail.com>
2022-06-17 14:13:11 -0400

    GOT PLAYSOUND WORKIN WOIOO!

--- java/src/main/java/conversationEngineLine/PlaySound.java
+++ java/src/main/java/conversationEngineLine/PlaySound.java
@@ -9,18 +9,18 @@ import conversationEngineImporter.NPC;
 
 public class PlaySound extends ConversationLine {
 
-	String command;
+	String sound;
 
-	public PlaySound(String command, ConverzationNode node) {
+	public PlaySound(String sound, ConverzationNode node) {
 		super(node);
-		if(command.charAt(0) == '/') {
-			command = command.substring(1);//if the command starts with a / remove it.
+		if(sound.charAt(0) == '/') {
+			sound = sound.substring(1);//if the command starts with a / remove it.
 		}
-		this.command = command;
+		this.sound = sound;
 	}
 
 	public String toCommand(HashMap<String, ConverzationNode> nodes, CEStory ceStory, NPC npc, LinkedList<String> condition, String con) {
-		return String.format("%srun stopsound @s\n%srun playsound %s voice @s\n",con, con, command);
+		return String.format("%srun stopsound @s\n%sat @e[type= villager, tag = %s] run playsound %s voice @s\n",con, con, npc.getTagName(), sound);
 	}
 
 }
