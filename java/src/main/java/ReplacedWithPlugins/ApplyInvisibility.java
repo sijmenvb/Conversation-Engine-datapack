0a1e4a9166191d2f50dbcf41730ca86bf9294d08
sijmen_v_b <sijmenvanbommel@gmail.com>
2024-01-25 03:29:04 +0100

    fixed timing to make a bit more responsive.

--- java/src/main/java/ReplacedWithPlugins/ApplyInvisibility.java
+++ java/src/main/java/ReplacedWithPlugins/ApplyInvisibility.java
@@ -6,7 +6,7 @@ public class ApplyInvisibility extends CEScheduledCommand {
 
 	public String toScheduledCommandWithComment() {
 		String comment = "# make invisible villagers invisible";
-		String command = "execute as @e[tag=CE_invisible] run data merge entity @s {NoGravity:0b,Silent:1b,active_effects:[{id:\"minecraft:invisibility\",amplifier:0b,duration:401,show_particles:0b}]}";
+		String command = "execute as @e[tag=CE_invisible] run data merge entity @s {NoGravity:0b,Silent:1b,active_effects:[{id:\"minecraft:invisibility\",amplifier:0b,duration:1000,show_particles:0b}]}";
 		return String.format("%s\n%s", comment, command);
 	}
 
